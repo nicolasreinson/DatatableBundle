8bdfa184459f771a0f26dff93bba2dde53c7e8f1
Nicolas Reinson <nicolas.reinson@travelsify.com>
2016-09-30 11:51:15 +0200

    remove useless query on Datatable execution and allow references by field name in cell view

--- Util/Factory/Query/DoctrineBuilder.php
+++ Util/Factory/Query/DoctrineBuilder.php
@@ -278,9 +278,8 @@ class DoctrineBuilder implements QueryInterface
         }
     }
 
+
     /**
-     * get data
-     *
      * @return array
      */
     public function getData()
@@ -328,7 +327,6 @@ class DoctrineBuilder implements QueryInterface
                     ->setFirstResult((int) $request->get('start'));
         }
 
-        $objects = $query->getResult(Query::HYDRATE_OBJECT);
         $maps = $query->getResult(Query::HYDRATE_SCALAR);
         $data = array();
 
@@ -357,7 +355,7 @@ class DoctrineBuilder implements QueryInterface
             $data[] = $item;
         }
 
-        return array($data, $objects);
+        return $data;
     }
 
     /**
